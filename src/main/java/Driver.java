a74b670113b570ec00269761254b30656fccfbe8
Scott Bouloutian <ScottBouloutian@gmail.com>
2016-08-13 18:16:43 -0400

    generate maze command

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,12 +1,12 @@
-import java.util.Scanner;
-
 public class Driver {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the maze generator!");
-        MazeEngine engine = new MazeEngine(10000);
+        int mazeSize = (args.length >= 1 && !args[0].isEmpty()) ? Integer.parseInt(args[0]) : 30;
+        System.out.println("Generating a " + mazeSize + "x" + mazeSize + " maze");
+        MazeEngine engine = new MazeEngine(mazeSize);
         engine.generateMaze();
-        engine.saveMaze("maze-10000.dat");
+        engine.saveMaze("maze.dat");
     }
 
 }
